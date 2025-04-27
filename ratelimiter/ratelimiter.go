5c3620b06c29d85a61dc3fb1c845e88c5e607f8b
root <root@vini.>
2025-04-27 12:55:00 -0400

    WIP problem 7: Add map initialization, nil checks, and partial reset/enforcement logic

--- ratelimiter/ratelimiter.go
+++ ratelimiter/ratelimiter.go
@@ -10,6 +10,13 @@ type accessTracker struct {
 	requestCounter int
 }
 
+func newAccessTracker(firstAccess time.Time, requestCounter int) *accessTracker {
+	return &accessTracker{
+		firstAccess:    firstAccess,
+		requestCounter: requestCounter,
+	}
+}
+
 type rateLimiter struct {
 	requestLimit int
 	duration     time.Duration
@@ -18,41 +25,43 @@ type rateLimiter struct {
 }
 
 func newRateLimiter(limit int, duration time.Duration) *rateLimiter {
-	newAccessTracker := make(map[string])
 	return &rateLimiter{
 		requestLimit: limit,
 		duration:     duration,
+		trackers:     make(map[string]*accessTracker),
 	}
 }
 
-func (rl *rateLimiter) checkTimeWindow(ip string) bool{
+func (rl *rateLimiter) checkTimeWindow(ip string) bool {
 	rl.mu.RLock()
 	defer rl.mu.RUnlock()
 	timeDelta := time.Now().Sub(rl.trackers[ip].firstAccess)
-	if timeDelta < rl.duration {
+	return timeDelta < rl.duration
+}
+
+func (rl *rateLimiter) resetTimeWindow(ip string) {
+	rl.mu.Lock()
+	rl.trackers[ip].requestCounter = 1
+	rl.trackers[ip].firstAccess = time.Now()
+	rl.mu.Unlock()
+}
+
+func (rl *rateLimiter) trackRequest(ip string) bool {
+	rl.mu.Lock()
+	defer rl.mu.Unlock()
+	ipTracker, check := rl.trackers[ip]
+	if !check {
+		firstAccess := time.Now()
+		requestCounter := 1
+		rl.trackers[ip] = newAccessTracker(firstAccess, requestCounter)
 		return true
 	}
-	return false
-}
 
-func (rl *rateLimiter) resetTimeWindow(ip string) bool {
-	time.Sleep(2 * time.Minute)
-	rl.trackers[ip].firstAccess = 0
-}
-
-func (rl *rateLimiter) trackRequest (ip string) {
-	rl.mu.Lock()
-	existingIp := rl.trackers[ip]
-	defer rl.mu.Unlock()
-
-	if existingIp.requestCounter == 0 {
-		rl.trackers[ip].firstAccess = time.Now()
-		rl.trackers[ip].requestCounter = 1
-	} 
-	if existingIp.requestCounter < rl.requestLimit && rl.checkTimeWindow(ip) {
+	if ipTracker.requestCounter < rl.requestLimit && rl.checkTimeWindow(ip) {
 		rl.trackers[ip].requestCounter++
+		return true
 	} else {
 		rl.resetTimeWindow(ip)
+		return false
 	}
-
 }
